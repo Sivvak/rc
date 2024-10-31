d16ba22661f51805b49f95ed6e1f5b03317f9911
Sivvak <pawel.7p@gmail.com>
2024-10-31 14:18:14 +0100

    add files from lab2

---
+++ docs/lab2/lab2.py
@@ -0,0 +1,74 @@
+import mujoco
+import matplotlib.pylab as plt
+
+
+def render_example():
+    for i in range(10):
+        xml = f"""
+    <mujoco>
+    <visual>
+        <global offwidth="1280" offheight="1280"/>
+    </visual>
+    <worldbody>
+        <light name="top" pos="0 0 1"/>
+        <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
+        <geom name="green_sphere" pos="{.2 + i / 10} .2 .2" size=".1" rgba="0 1 0 1"/>
+    </worldbody>
+    </mujoco>
+    """
+        model = mujoco.MjModel.from_xml_string(xml)
+        data = mujoco.MjData(model)
+        renderer = mujoco.Renderer(model, 1280, 1280)
+
+        mujoco.mj_forward(model, data)
+        renderer.update_scene(data)
+        plt.imsave(f"frame_{i:03d}.png", renderer.render())
+        renderer.close()
+
+
+def generate_world_scene(x_car, y_car, car_rot, rader_rot):
+    xml = f"""
+<?xml version="1.0" ?>
+<mujoco>
+    <worldbody>
+         <body name="floor" pos="0 0 -0.1">
+            <geom size="2.0 2.0 0.02" rgba="0.2 0.2 0.2 1" type="box"/>
+        </body>
+        <body name="x_arrow" pos="0.5 0 0">
+            <geom size="0.5 0.01 0.01" rgba="1 0 0 0.5" type="box"/>
+        </body>
+        <body name="y_arrow" pos="0 0.5 0">
+            <geom size="0.01 0.5 0.01" rgba="0 1 0 0.5" type="box"/>
+        </body>
+        <body name="z_arrow" pos="0 0 0.5">
+            <geom size="0.01 0.01 0.5" rgba="0 0 1 0.5" type="box"/>
+        </body>
+
+        <body name="car" pos="{x_car} {y_car} 0.1" axisangle="0 0 1 0">
+            <geom size="0.2 0.1 0.02" rgba="1 1 1 0.9" type="box"/>
+        </body>
+        <body name="wheel_1" pos="{x_car + 0.1} {y_car + 0.1} 0.1" axisangle="1 0 0 90">
+            <geom size="0.07 0.01" rgba="1 1 1 0.9" type="cylinder"/>
+        </body>
+        <body name="wheel_2" pos="{x_car - 0.1} {y_car + 0.1} 0.1" axisangle="1 0 0 90">
+            <geom size="0.07 0.01" rgba="1 1 1 0.9" type="cylinder"/>
+        </body>
+        <body name="wheel_3" pos="{x_car + 0.1} {y_car - 0.1} 0.1" axisangle="1 0 0 90">
+            <geom size="0.07 0.01" rgba="1 1 1 0.9" type="cylinder"/>
+        </body>
+        <body name="wheel_4" pos="{x_car - 0.1} {y_car - 0.1} 0.1" axisangle="1 0 0 90">
+            <geom size="0.07 0.01" rgba="1 1 1 0.9" type="cylinder"/>
+        </body>
+
+        <body name="radar_1" pos="{x_car} {y_car - 0.1} 0.2" axisangle="1 0 0 30">
+            <geom size="0.01 0.01 0.1" rgba="1 1 1 1" type="box"/>
+        </body>
+        <body name="radar_2" pos="{x_car} {y_car - 0.15} 0.29" axisangle="1 0 0 30">
+            <geom size="0.03 0.01" rgba="1 0 0 1" type="cylinder"/>
+        </body>
+    </worldbody>
+</mujoco>"""
+
+
+def render_driving_car():
+    pass
