24ab1abc09330d126d00a156290849f18d4aad80
Sivvak <pawel.7p@gmail.com>
2024-11-26 18:10:43 +0100

    add things done for lab2&3 scenarios

--- docs/lab2/lab2.py
+++ docs/lab2/lab2.py
@@ -1,5 +1,6 @@
 import mujoco
 import matplotlib.pylab as plt
+import numpy as np
 
 
 def render_example():
@@ -30,9 +31,12 @@ def generate_world_scene(x_car, y_car, car_rot, rader_rot):
     xml = f"""
 <?xml version="1.0" ?>
 <mujoco>
+    <visual>
+        <global offwidth="1280" offheight="1280" fovy="35"/>
+    </visual>
     <worldbody>
          <body name="floor" pos="0 0 -0.1">
-            <geom size="2.0 2.0 0.02" rgba="0.2 0.2 0.2 1" type="box"/>
+            <geom size="1.0 1.0 0.02" rgba="0.2 0.2 0.2 1" type="box"/>
         </body>
         <body name="x_arrow" pos="0.5 0 0">
             <geom size="0.5 0.01 0.01" rgba="1 0 0 0.5" type="box"/>
@@ -60,15 +64,36 @@ def generate_world_scene(x_car, y_car, car_rot, rader_rot):
             <geom size="0.07 0.01" rgba="1 1 1 0.9" type="cylinder"/>
         </body>
 
-        <body name="radar_1" pos="{x_car} {y_car - 0.1} 0.2" axisangle="1 0 0 30">
+        <body name="radar_1" pos="{x_car} {y_car + 0.1} 0.2" axisangle="1 0 0 330">
             <geom size="0.01 0.01 0.1" rgba="1 1 1 1" type="box"/>
         </body>
-        <body name="radar_2" pos="{x_car} {y_car - 0.15} 0.29" axisangle="1 0 0 30">
+        <body name="radar_2" pos="{x_car} {y_car + 0.15} 0.29" axisangle="1 0 0 330">
             <geom size="0.03 0.01" rgba="1 0 0 1" type="cylinder"/>
         </body>
     </worldbody>
 </mujoco>"""
 
+    return mujoco.MjModel.from_xml_string(xml)
+
 
 def render_driving_car():
-    pass
+    center = (-1, 0)
+
+    angles = np.linspace(3 / 2 * np.pi, 2 * np.pi, 10)
+
+    for i, angle in enumerate(angles):
+        x = -1 + np.cos(angle)
+        y = np.sin(angle)
+
+        model = generate_world_scene(x, y, 0, 0)
+
+        data = mujoco.MjData(model)
+        renderer = mujoco.Renderer(model, 1280, 1280)
+
+        mujoco.mj_forward(model, data)
+        renderer.update_scene(data)
+        plt.imsave(f"cm_frame_{i:03d}.png", renderer.render())
+        renderer.close()
+
+
+render_driving_car()
--- docs/lab3/mujoco/stereo_vision.py
+++ docs/lab3/mujoco/stereo_vision.py
@@ -3,6 +3,34 @@ from PIL import Image
 import cv2
 import numpy as np
 
+# camera paramas
+resolution = (1280, 1280)
+ox = resolution[0] / 2
+oy = resolution[1] / 2
+
+fovy = np.pi / 2
+f = (ox / np.tan(fovy / 2)).item()
+
+
+def detect_contour(img, lower, upper):
+    hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    kernel = np.ones((5, 5), "uint8")
+
+    mask = cv2.inRange(hsvImg, lower, upper)
+    mask = cv2.dilate(mask, kernel)
+
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return cv2.boundingRect(contours[0])
+
+
+def to_3D(x1, y1, w1, h1, x2, y2, w2, h2, z_offset):
+    x_3D = baseline * (x1 + w1 / 2 - ox) / (x1 + w1 / 2 - (x2 + w2 / 2))
+    y_3D = baseline * f * (y1 + h1 / 2 - oy) / (f * (x1 + w1 / 2 - (x2 + w2 / 2)))
+    z_3D = baseline * f / (x1 + w1 / 2 - (x2 + w2 / 2))
+
+    return [x_3D, y_3D, z_3D - z_offset / 2]
+
+
 # distance between two cameras
 baseline = 0.1
 
@@ -10,23 +38,35 @@ sphere_size = 0.01
 box1_size = 0.05
 box2_size = 0.02
 
-resolution = (1280, 1280)
+img1 = cv2.imread("rc/docs/lab3/mujoco/left.png")
+img2 = cv2.imread("rc/docs/lab3/mujoco/right.png")
 
-img1 = cv2.imread('left.png')
-img2 = cv2.imread('right.png')
+# TODO: find the positions of the objects and reconstruct the images
+green_lower = np.array([36, 25, 25], np.uint8)
+green_upper = np.array([70, 255, 255], np.uint8)
 
-#TODO: find the positions of the objects and reconstruct the images
+red_lower = np.array([136, 87, 111], np.uint8)
+red_upper = np.array([180, 255, 255], np.uint8)
+
+blue_lower = np.array([94, 80, 2], np.uint8)
+blue_upper = np.array([120, 255, 255], np.uint8)
 
 # red sphere
-pos_sphere = (...)
+# x1, y1, w1, h1 = detect_contour(img1, red_lower, red_upper)
+# x2, y2, w2, h2 = detect_contour(img2, red_lower, red_upper)
+# pos_sphere = to_3D(x1, y1, w1, h1, x2, y2, w2, h2, sphere_size)
 
 # green box
-pos_box1 = (...)
+x1, y1, w1, h1 = detect_contour(img1, green_lower, green_upper)
+x2, y2, w2, h2 = detect_contour(img2, green_lower, green_upper)
+pos_box1 = to_3D(x1, y1, w1, h1, x2, y2, w2, h2, box1_size)
 
 # blue box
-pos_box2 = (...)
+x1, y1, w1, h1 = detect_contour(img1, blue_lower, blue_upper)
+x2, y2, w2, h2 = detect_contour(img2, blue_lower, blue_upper)
+pos_box2 = to_3D(x1, y1, w1, h1, x2, y2, w2, h2, box2_size)
 
-xml_string =f"""\
+xml_string = f"""\
 <mujoco model="simple_scene">
       <visual>
      <global offwidth="{resolution[0]}" offheight="{resolution[1]}"/>
@@ -38,10 +78,6 @@ xml_string =f"""\
 
     <worldbody>
         <geom type="plane" size="1 1 0.1" material="plane_material"/>
-
-        <body pos="{pos_sphere[0]} {pos_sphere[1]} {pos_sphere[2]}">
-            <geom type="sphere" size="{sphere_size}" rgba="1 0 0 1"/>
-        </body>
 
         <body pos="{pos_box1[0]} {pos_box1[1]} {pos_box1[2]}">
             <geom type="box" size="0.05 0.05 0.05" rgba="0 1 0 1"/>
